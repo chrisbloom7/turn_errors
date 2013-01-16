bcd53a90c402d6e161d2ad7e854a17912365ac9f
Chris Bloom <chrisbloom7@gmail.com>
2013-01-16 08:46:16 -0500

    Project skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TurnErrors::Application.config.secret_token = 'a2d10b73181b7fb67ac9dfde9315b3b14f3299a3a672aae2bbf22b1e99449d559aad6a267aeb32c94e9aa90838275c21197aa8a75c2eb445d4fc615a95128745'
